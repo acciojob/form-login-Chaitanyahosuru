6ac044c909a644c9a8cc0bfe293324f847339ef5
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-09-17 20:05:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function getFormvalue() {
-    //Write your code here
-	var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
+function getFormvalue(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    
+    // Get the form element
+    var form = document.getElementById('form1');
+    
+    // Extract the first and last name values
+    var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
     
     // Create the full name string
     var fullName = firstName + ' ' + lastName;
     
     // Display the full name in an alert
     alert(fullName);
-
 }
