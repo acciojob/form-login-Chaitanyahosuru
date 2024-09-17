d68238d8c0d256edbae8a4019fb796c25ca8731a
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-09-17 20:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+	var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    
+    // Create the full name string
+    var fullName = firstName + ' ' + lastName;
+    
+    // Display the full name in an alert
+    alert(fullName);
 
 }
